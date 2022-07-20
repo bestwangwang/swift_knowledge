8fe804e563fa96ded4ed0af0111e7abd1340b7cd
benwang <2671373539@qq.com>
2022-07-20 09:31:53 +0800

    add swift project

---
+++ swift_knowledge/swift_knowledge/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  swift_knowledge
+//
+//  Created by benwang on 2022/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let a = [1]
+        let b = [2]
+    }
+
+
+
+}
+
