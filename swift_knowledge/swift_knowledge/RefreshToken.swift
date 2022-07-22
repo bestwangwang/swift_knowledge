14b9a4ba7c82a6f1363f7408f2c42aa45cc18b61
benwang <2671373539@qq.com>
2022-07-22 13:49:21 +0800

    rxswift refresh token

--- swift_knowledge/swift_knowledge/RefreshToken.swift
+++ swift_knowledge/swift_knowledge/RefreshToken.swift
@@ -7,30 +7,122 @@
 
 import Foundation
 import RxSwift
+import RxRelay
 
 struct Token {
     static let shared = Token()
 
-    let refreshStatus = BehaviorSubject(value: false)
-
-
+    /// 是否正在刷新
+    let refreshStatus = BehaviorRelay(value: false)
 }
 
 class LogicService {
     static let shared = LogicService()
 
-//    let refreshToken = Token.shared.refreshStatus.
+    let disposeBag = DisposeBag()
+
+    //    let refreshToken = Token.shared.refreshStatus.
 
     let taskQueue = DispatchQueue(label: "logic", attributes: .concurrent)
 
+    let lock = NSRecursiveLock()
 
+    func request(url: String) -> Single<String> {
+        /// a b c
+        /// a 请求
+        return Token.shared.refreshStatus
+            .distinctUntilChanged()
+            // 正在刷新的等待，丢弃信号
+            .filter {
+                !$0
+            }
+            .first()
+//            .map { _ in }
+            // 请求业务接口
+            .flatMap { _ in
+                self._request(url: url)
+            }
+            // 接口数据处理
+            .flatMap { data -> Single<String> in
 
+                // 错误处理：Token过期
+                // 需要刷新
+                if data.isEmpty {
+                    defer { self.lock.unlock() }
+                    self.lock.lock()
 
+                    // 没有刷新，则开始刷新
+                    if !Token.shared.refreshStatus.value {
+                        Token.shared.refreshStatus.accept(true)
+                        return self._request()
+                            .flatMap { _ in
+                                Token.shared.refreshStatus.accept(false)
+                                return self._request(url: url)
+                            }
+                    }
+                    return self._request(url: url)
+                }
 
-
-    func _request(url: String, handler: @escaping (String) -> Void) -> Void {
-//        taskQueue.asyncAfter(deadline: .now() + 1) {
-//            handler(url)
-//        }
+                // 正确处理：结果透传
+                return Single<String>.just(data)
+            }
     }
+}
+
+extension LogicService {
+
+    func _request() -> Single<Bool> {
+        return Single.create { [weak self] observer in
+            let item = DispatchWorkItem {
+//                print("token: thread-\(Thread.current)")
+                print("Token刷新完毕")
+                tokenable = true
+                observer(.success(true))
+            }
+            print("正在刷新Token...")
+            self?.taskQueue.asyncAfter(deadline: .now() + 1, execute: item)
+
+            return Disposables.create {
+                item.cancel()
+            }
+        }
+//        .observe(on: MainScheduler.instance)
+    }
+
+    func _request(url: String) -> Single<String> {
+        return Single.create { [weak self] observer in
+
+            let item = DispatchWorkItem {
+                if tokenable {
+                    //print("logic: thread-\(Thread.current)")
+                    print("数据请求完毕：\(url)")
+                    observer(.success(url))
+                } else {
+                    print("Token过期：\(url)")
+                    observer(.success(""))
+                }
+            }
+
+            print("正在请求数据：\(url)")
+            self?.taskQueue.asyncAfter(deadline: .now() + 1, execute: item)
+
+            return Disposables.create {
+                item.cancel()
+            }
+        }
+//        .observe(on: MainScheduler.instance)
+    }
+}
+
+var tokenable: Bool {
+    get {
+        UserDefaults.standard.bool(forKey: "token")
+    }
+    set {
+        UserDefaults.standard.set(newValue, forKey: "token")
+    }
+}
+
+enum LogicError: Error {
+    case invalidToken
 }
--- swift_knowledge/swift_knowledge/ViewController.swift
+++ swift_knowledge/swift_knowledge/ViewController.swift
@@ -12,24 +12,93 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    var disposeBag  = DisposeBag()
+    var disposable: Disposable?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
     }
 
+    var number = 0
 
     @IBAction func tappedOne(_ sender: Any) {
-
+        simultaneousCase()
     }
 
-
     @IBAction func tappedTwo(_ sender: Any) {
-
+        onebyoneCase()
     }
 
     @IBAction func tappedThree(_ sender: Any) {
+        tokenable = false
+    }
+}
 
+extension ViewController {
+
+    /// 同时异步请求
+    /// 测试是否重复刷新token
+    func simultaneousCase() {
+        number = number + 1
+
+        LogicService.shared.request(url: "R\(number)")//.debug()
+            .subscribe(onSuccess: { ret in
+                print("request token: \(ret) \(Thread.current)")
+            })
+            .disposed(by: disposeBag)
+
+
+        number = number + 1
+
+        LogicService.shared.request(url: "R\(number)")//.debug()
+            .subscribe(onSuccess: { ret in
+                print("request token: \(ret) \(Thread.current)")
+            })
+            .disposed(by: disposeBag)
+
+
+        number = number + 1
+
+        LogicService.shared.request(url: "R\(number)")//.debug()
+            .subscribe(onSuccess: { ret in
+                print("request token: \(ret) \(Thread.current)")
+            })
+            .disposed(by: disposeBag)
+    }
+
+    /// 依次请求接口
+    /// 测试正在刷新时，是否会等待刷新完毕后再请求
+    func onebyoneCase() {
+        number = number + 1
+
+        LogicService.shared.request(url: "R\(number)")//.debug()
+            .subscribe(onSuccess: { ret in
+                print("request token: \(ret) \(Thread.current)")
+            })
+            .disposed(by: disposeBag)
+
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.number = self.number + 1
+
+            LogicService.shared.request(url: "R\(self.number)")//.debug()
+                .subscribe(onSuccess: { ret in
+                    print("request token: \(ret) \(Thread.current)")
+                })
+                .disposed(by: self.disposeBag)
+        }
+
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.number = self.number + 1
+            
+            LogicService.shared.request(url: "R\(self.number)")//.debug()
+                .subscribe(onSuccess: { ret in
+                    print("request token: \(ret) \(Thread.current)")
+                })
+                .disposed(by: self.disposeBag)
+        }
     }
 
 }
